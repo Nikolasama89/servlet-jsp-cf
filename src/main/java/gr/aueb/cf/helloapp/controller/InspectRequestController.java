e9ab3be996c7eccae4f45d622e1b381047028fd7
Nikos <niqos89@gmail.com>
2025-03-26 20:58:02 +0200

    feat: cookies-based auth and conditionals in jsp pages

--- src/main/java/gr/aueb/cf/helloapp/controller/InspectRequestController.java
+++ src/main/java/gr/aueb/cf/helloapp/controller/InspectRequestController.java
@@ -2,10 +2,7 @@ package gr.aueb.cf.helloapp.controller;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.*;
 
 import java.io.IOException;
 
@@ -15,7 +12,23 @@ public class InspectRequestController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.getWriter().write("Session id: " + req.getSession());
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        HttpSession session = req.getSession(true);     // false: get current session (if exists) else return null
+
+        if (username != null && username.equals("nikos") && password != null && password.equals("12345")) {
+            session.setAttribute("username", username);
+            session.setAttribute("role", "TEACHER");
+
+        }
+
+        resp.sendRedirect("/school-app/teachers");
+
+
+
+
+        resp.getWriter().write("Session id: " + req.getSession() + "\n");
 
         Cookie[] cookies = req.getCookies();
         if (cookies != null) {
